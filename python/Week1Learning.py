31151506338502e42b771c30ec79e6c0c0b8a13d
Melodi <83541357+Melodi3D@users.noreply.github.com>
2026-07-23 19:47:39 -0400

    Add files via upload

---
+++ python/Week1Learning.py
@@ -0,0 +1,62 @@
+
+
+#*******************************************************************************
+# 02. EPIC MOMENT
+#*******************************************************************************
+# Print out your very first Hello World.
+
+print("Hello World!")
+
+
+
+
+
+
+
+
+#*******************************************************************************
+# 03. DATA TYPES
+#*******************************************************************************
+# Identify the following data types (without looking them up).
+# Note the data type as a comment in the same line next to them.
+
+True
+# Boolean
+4
+# Integer
+False
+# Boolean
+'You start to learn Python'
+# String
+324564
+# Integer
+"and more ..."
+# String
+0.4324322
+# Float
+None
+# Boolean
+
+
+
+
+
+
+
+
+
+#*******************************************************************************
+# 04. VARIABLES
+#*******************************************************************************
+# Create a variable for each data type (5) and fill it with content.
+# Print them all.
+
+# TIP: Use lower case for variables
+
+Money = 1.29
+PI = 3.14159265
+Close_Friends = 3
+Kind = True
+Evil = False
+First_Name = 'Melodi'
+Last_Name = "Clark"
